74be574fba4f509da69b4bd1fec9648112aa0efe
RayAndrade <TheRayCode@RayAndrade.com>
2020-01-25 08:30:19 -0800

    C++ Tutorial for Beginners 4 - Outputting Text

---
+++ Outputing/src/Outputing.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : Outputing.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : C++ Tutorial for Beginners 4 - Outputting Text
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "This is some text " << flush;
+	cout << "This is some more text after a flush " << endl;
+	cout << "This is even more text" << endl;
+	cout << "Banana, " << "Apple, " << "Orange." << endl;
+
+	return 0;
+}
