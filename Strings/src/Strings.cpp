8ac1a834a9d401f99f4d310ac73e8194f571e220
RayAndrade <TheRayCode@RayAndrade.com>
2020-01-25 11:46:05 -0800

    C++Tutorial for Beginners 6 - Strings

---
+++ Strings/src/Strings.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : Strings.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : C++Tutorial for Beginners 6 - Strings
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	string text1 = "Hello";
+	string text2 = "Ray";
+	string text3 = text1 + " " +text2;
+	cout << text1 << " " << text2 << endl;
+	cout << text3 << endl;
+	return 0;
+}
