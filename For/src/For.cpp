22d4177bd87acbdaa12789b3f57ec078b1a126df
RayAndrade <TheRayCode@RayAndrade.com>
2020-01-26 14:04:42 -0800

    C++ Tutorial for Beginners 18 - For Loops

---
+++ For/src/For.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : For.cpp
+// Author      : Ray Andrade
+// Version     :
+// Copyright   : 
+// Description : C++ Tutorial for Beginners 18 - For Loops
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	for(int i = 0; i < 10; i++){
+		cout << "The value of i is :" << i << endl;
+	}
+
+	return 0;
+}
