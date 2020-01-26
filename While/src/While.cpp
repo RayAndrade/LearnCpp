6f5a3389c7b066a12b95cc03e46cc0bee3f6333d
RayAndrade <TheRayCode@RayAndrade.com>
2020-01-26 13:36:53 -0800

    C++Tutorial for Beginners 16 - "While" Loops

---
+++ While/src/While.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : While.cpp
+// Author      : Ray Andrade
+// Version     :
+// Copyright   : 
+// Description : C++Tutorial for Beginners 16 - "While" Loops
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i = 10;
+
+	while(i >= 0){
+		cout << "the count is: " << i << endl;
+		i --;
+	}
+
+	return 0;
+}
