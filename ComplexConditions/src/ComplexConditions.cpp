e0c7c0fcca581e509ce73a339e26f2b8d9932aaf
RayAndrade <TheRayCode@RayAndrade.com>
2020-01-25 16:44:51 -0800

    C++ Tutorial for Beginners 15 - Complex Conditions

---
+++ ComplexConditions/src/ComplexConditions.cpp
@@ -0,0 +1,33 @@
+//============================================================================
+// Name        : ComplexConditions.cpp
+// Author      : Ray Andrade
+// Version     :
+// Copyright   : 
+// Description : C++ Tutorial for Beginners 15 - Complex Conditions
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+/*
+ * == equaithy test
+ * != not equals
+ * < less then
+ * > greater than
+ * <= less than OR equal to
+ * >= greater than OR equal to
+ *
+ */
+
+int main() {
+	int value1 = 7;
+	int value2 = 4;
+
+	if(value1 < 8){
+		cout << "contition 1 is true" << endl;
+	}
+	else {
+		cout << "condition 1 is false" << endl;
+	}
+	return 0;
+}
