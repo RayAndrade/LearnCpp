9b94d11f3195f39113ba0bd74b0e068c8631b269
RayAndrade <TheRayCode@RayAndrade.com>
2020-01-31 14:34:16 -0800

    C++Tutorial for Beginners 45 - Copy Constructors; Creating Copies of Objects

---
+++ ViewOperator/src/ViewOperator.cpp
@@ -0,0 +1,28 @@
+//============================================================================
+// Name        : ViewOperator.cpp
+// Author      : Ray Andrade
+// Version     :
+// Copyright   : 
+// Description : C++Tutorial for Beginners 46 - The New Operator; Allocating Memory in C++
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+class Animal {
+	private:
+		string name;
+public:
+
+	void setName(string name){ this->name = name; };
+	void speak() const {cout << "Hello I am " << name << endl;};
+};
+
+
+int main() {
+
+
+	return 0;
+}
