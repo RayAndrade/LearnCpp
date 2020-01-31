4858613134355c55cc7d5c07be4f2fec1619423c
RayAndrade <TheRayCode@RayAndrade.com>
2020-01-30 19:21:44 -0800

    C++Tutorial for Beginners 43 - References; Nicer than Pointers!

---
+++ References/src/References.cpp
@@ -0,0 +1,31 @@
+//============================================================================
+// Name        : References.cpp
+// Author      : Ray Andrade
+// Version     :
+// Copyright   : 
+// Description : C++Tutorial for Beginners 43 - References; Nicer than Pointers!
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+void changeSomething(double value){
+	value = 123.4;
+}
+
+int main() {
+
+	int value1 = 8;
+	int &value2 = value1;
+	value2 = 10;
+
+	cout << "value1: " << value1 << endl;
+	cout << "value2: " << value2 << endl;
+	cout << "&value2: " << &value2 << endl;
+
+	double value = 4.321;
+	changeSomething(value);
+	cout << value << endl;
+
+	return 0;
+}
